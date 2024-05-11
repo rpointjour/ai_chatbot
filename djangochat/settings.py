41e79b78e7b8256d32583e560815d03e802f098d
rjpking92 <roody.jerry@yahoo.com>
2024-05-11 17:33:42 -0400

    added static image

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -115,6 +115,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
 
 STATIC_URL = 'static/'
 
