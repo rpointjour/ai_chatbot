9a783d2dfca2ca3c7dbf6cf941cb14b99d557e20
rjpking92 <roody.jerry@yahoo.com>
2024-05-23 18:25:44 -0400

    preparing for deployment

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -117,11 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+import os
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
