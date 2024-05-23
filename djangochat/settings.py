219c51d821520eaa1352fb13c59eecae69b705a3
rjpking92 <roody.jerry@yahoo.com>
2024-05-23 18:49:23 -0400

    preparing for deployment

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -76,10 +76,7 @@ WSGI_APPLICATION = 'djangochat.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 import dj_database_url
